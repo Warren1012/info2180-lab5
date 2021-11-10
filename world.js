34ad32fb652c365186f98798a3e0795c33ce330e
Annwar Greenland <annwar101@gmail.com>
2021-11-10 00:45:02 -0500

    Final

--- world.js
+++ world.js
@@ -1,6 +1,7 @@
 window.onload =function()
 {
     var loadsearch = document.getElementById('lookup');
+    var citysearch = document.getElementById('cities');
     var httpRequest = new XMLHttpRequest();
 
     loadsearch.addEventListener('click', function(e)
@@ -16,6 +17,30 @@ window.onload =function()
                 var output = document.getElementById("result");
                 var response = httpRequest.responseText;
                 output.innerHTML = response;    
+            }
+            else
+            {
+                var msg ="problem";
+                msg.innerHTML = msg;
+            }   
+        };
+        httpRequest.open('GET', url);
+        httpRequest.send(); 
+    });
+
+    citysearch.addEventListener('click', function(c)
+    {
+        console.log('button clicked')
+        var cities= document.getElementById('country').value;
+        var url = "http://localhost/info2180-lab5/world.php?context="+cities;
+        c.preventDefault();
+        httpRequest.onreadystatechange = function() 
+        {
+            if(httpRequest.readyState == XMLHttpRequest.DONE && httpRequest.status == 200)
+            {  
+                var ans = document.getElementById("result");
+                var response2 = httpRequest.responseText;
+                ans.innerHTML = response2;    
             }
             else
             {
--- world.php
+++ world.php
@@ -3,18 +3,52 @@ $host = 'localhost';
 $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
+$context='holder';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $country=$_GET['country'];
+$context=$_GET['cities'];
 htmlspecialchars($country);
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+
+if($context != "cities")
+{
+  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+  
+}
+else
+{
+  $stmt= $conn->query("SELECT cities.district, cities.population, cities.name FROM countries INNER JOIN cities ON 
+  countries.code = cities.country_code WHERE countries.name LIKE '%$country%'");
+}
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+header('Access-Control-Allow-Origin: *');
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+<table>
+<?php if ($context != "cities"){
+ print"<tr>
+  <th>Name</th>
+  <th>Continent</th>
+  <th>Indepence</th>
+  <th>Head of State</th>
+</tr>";
 
+ foreach ($results as $row):?>
+<tr><?= "<td>".$row['name']."</td>"."<td>". $row['continent']."</td>"."<td>".$row['independence_year']."</td>"."<td>" . $row['head_of_state']. "</td>";?></tr>
+<?php endforeach;
+     }?>
+
+   </table>
+<table>
+   <?php if ($context == "cities"){
+ print"<tr>
+  <th>Name</th>
+  <th>District</th>
+  <th>Population</th>
+</tr>";
+
+ foreach ($results as $row):?>
+<tr><?= "<td>".$row['name']."</td>"."<td>". $row['district']."</td>"."<td>".$row['population']."</td>";?></tr>
+<?php endforeach;
+     }?>
+</table>
